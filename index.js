68288cca0d153c601d57a80677d06fc3e0d9b491
thegamebegins25 <wdppostcard@gmail.com>
2022-04-28 07:09:53 -0400

    different gif

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function change(img) {
-    document.getElementById("image").src = "img/" + img + ".jpg";
+    var x = "";
+    if (img < 100 && img > 9) {
+      var x = "0";
+    }
+    if (img < 10) {
+      var x = "00";
+    }
+    var img = "img/" + x + img + ".jpg";
+    document.getElementById("image").src = img;
+    console.log(img);
 }
 
 var slider = document.getElementById("slider");
@@ -7,6 +16,7 @@ var slider = document.getElementById("slider");
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   console.log(this.value);
+
   change(this.value);
   
 }
